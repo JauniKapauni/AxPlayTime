875e2814c6448fb50a568a8b98a40212ef6c0bf2
JauniKapauni <riches-goods.54@icloud.com>
2026-05-26 17:13:08 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axplaytime/AxPlayTime.java
+++ src/main/java/de/jaunikapauni/axplaytime/AxPlayTime.java
@@ -1,12 +1,27 @@
 package de.jaunikapauni.axplaytime;
 
+import de.jaunikapauni.axplaytime.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxPlayTime extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1()){
+                Bukkit.getLogger().severe("Failed to create db table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
