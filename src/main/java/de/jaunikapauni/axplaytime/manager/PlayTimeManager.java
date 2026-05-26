be9b15676bc71cb8c87932ecbfac46f71302e81c
JauniKapauni <riches-goods.54@icloud.com>
2026-05-26 17:30:38 +0200

    feat: add persistence

--- src/main/java/de/jaunikapauni/axplaytime/manager/PlayTimeManager.java
+++ src/main/java/de/jaunikapauni/axplaytime/manager/PlayTimeManager.java
@@ -3,6 +3,10 @@ package de.jaunikapauni.axplaytime.manager;
 import de.jaunikapauni.axplaytime.AxPlayTime;
 import org.bukkit.entity.Player;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -29,9 +33,31 @@ public class PlayTimeManager {
         return endTime.get(p.getUniqueId()) - startTime.get(p.getUniqueId());
     }
     public void savePlaytimeDB(Player p){
-        UUID uuid = p.getUniqueId();
-        long sessionTime = getDelta(p);
-        long currentTotal = playtime.getOrDefault(uuid, 0L);
-        playtime.put(uuid, currentTotal + sessionTime);
+        long totalSave = playtime.getOrDefault(p.getUniqueId(), 0L);
+        try(Connection conn = reference.getDatabaseManager().getConnection()){
+            try(PreparedStatement ps = conn.prepareStatement("REPLACE INTO players(uuid, playtime) VALUES (?, ?)")){
+                ps.setString(1, p.getUniqueId().toString());
+                ps.setLong(2, totalSave);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Long loadPlayerPlaytime(UUID uuid){
+        try(Connection conn = reference.getDatabaseManager().getConnection()){
+            try(PreparedStatement ps = conn.prepareStatement("SELECT playtime FROM players WHERE uuid = ?")){
+                ps.setString(1, uuid.toString());
+                try(ResultSet rs = ps.executeQuery()){
+                    if(rs.next()){
+                        return rs.getLong("playtime");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0L;
     }
 }
