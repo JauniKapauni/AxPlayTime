bf9b481f818b04b363096569e7c34e48da9e275a
JauniKapauni <riches-goods.54@icloud.com>
2026-06-05 13:59:52 +0200

    fix: add close of conns

--- src/main/java/de/jaunikapauni/axplaytime/AxPlayTime.java
+++ src/main/java/de/jaunikapauni/axplaytime/AxPlayTime.java
@@ -52,5 +52,6 @@ public final class AxPlayTime extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        databaseManager.close();
     }
 }
--- src/main/java/de/jaunikapauni/axplaytime/manager/DatabaseManager.java
+++ src/main/java/de/jaunikapauni/axplaytime/manager/DatabaseManager.java
@@ -35,6 +35,12 @@ public class DatabaseManager {
         return hikari.getConnection();
     }
 
+    public void close(){
+        if(hikari != null && !hikari.isClosed()){
+            hikari.close();
+        }
+    }
+
     public boolean initDatabaseTable1(){
         try(Connection conn = getConnection()){
             try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS players(uuid VARCHAR(255), playtime LONG)")){
