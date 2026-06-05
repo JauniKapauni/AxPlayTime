b2edd4993d0159f9a73581fdacac105c12fc8e7e
JauniKapauni <riches-goods.54@icloud.com>
2026-06-05 13:58:39 +0200

    fix: fix playtime command output

--- src/main/java/de/jaunikapauni/axplaytime/command/PlayTimeCommand.java
+++ src/main/java/de/jaunikapauni/axplaytime/command/PlayTimeCommand.java
@@ -23,7 +23,7 @@ public class PlayTimeCommand implements CommandExecutor {
         long sessionStart = reference.getPlayTimeManager().getStartTime().getOrDefault(uuid, System.currentTimeMillis());
         long currentSessionTime = System.currentTimeMillis() - sessionStart;
         long totalPlaytime = totalSaved + currentSessionTime;
-        p.sendMessage("Your playtime: " + (totalPlaytime / 1000) + "s");
+        p.sendMessage("Your playtime: " + reference.getPlayTimeManager().formatPlaytime(totalPlaytime) + "s");
         return true;
     }
 }
